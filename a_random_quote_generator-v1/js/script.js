f4c893851abcc711027decca9c14b1a82cd2c5da
David Quilici <davidmquilici@yahoo.com>
2018-08-01 19:42:42 -0700

    Project: Random Quote Generator
    Using JavaScript to create a random quote generator.

---
+++ a_random_quote_generator-v1/js/script.js
@@ -0,0 +1,41 @@
+// FSJS - Random Quote Generator
+
+// Create the array of quote objects and name it quotes
+const quotes = [
+{   quote: `Do or do not, there is not try.`,
+    source: `Yoda, Jedi Master`,
+    citation: `George Lucas, Star Wars: The Empire Strikes Back.`,
+    year: 1980 
+}
+{   quote: `Love your neighbor as yourself.`,
+    source: `Jesus of Nazareth`,
+    citation: `Matthew 19:19b`,
+    year: `1st Century AD`
+}
+{   quote: `My bed is a magical place where I suddenly remember everything I forgot to do.`,
+    source: `Anonymous`,
+}
+{   quote: `Deeds will not be less valiant because they are unpraised.`,
+    source: `Aragorn, King of Gondor`,
+    citation: `J.R.R. Tolkien, The Return of the King`,
+    year: 1955
+}
+{   quote: `You don't get to choose not to pay a price, you only get to choose which price you pay.`,
+    source: `Jordan B. Peterson`,
+    citation: `12 Rules for Life`,
+    year: 2018
+}
+];
+
+
+// Create the getRandomQuote function and name it getRandomQuote
+
+
+
+// Create the printQuote function and name it printQuote
+
+
+
+// This event listener will respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
